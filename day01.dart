2474615fbc77c2b9cb3cc6fb10462756d4f0e3f0
jkoenig <julian.koenig@js-soft.com>
2022-12-02 08:19:36 +0100

    chore: remove unnecessary extension

--- day01.dart
+++ day01.dart
@@ -9,11 +9,11 @@ main(List<String> args) {
         .asIntList(firstSplit: "\n\n", secondSplit: "\n")
         .map((e) => e.reduce((value, element) => value + element))
         .toList()
-        .sortInplace((a, b) => b - a),
+      ..sort((a, b) => b - a),
     part1,
     part2,
   );
 }
 
 int part1(List<int> input) => input[0];
-int part2(List<int> input) => input[0] + input[1] + input[2];
+int part2(List<int> input) => input.take(3).sum;
